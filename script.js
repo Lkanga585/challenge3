58bb3a1e84f80390cb16df0b329f2a025190d789
Lkanga585 <lfmkanga@gmail.com>
2023-09-06 23:06:56 -0700

    changes

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var number ='0123456789'
+var letters ='abced'
+var capletters = letters.toUpperCase()
+var special ="!@#$%%"
+var candidates=''
 
+
+function generatePassword(){
+  var password =''
+for(var i =0; i<mylength; i++){
+  var index= parseInt(Math.random()* candidates.length)
+password+= candidates[index]
+console.log (i, ' ',  password )
+}
+return password  
+}
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var mylength= parseInt(prompt('how long is your password'))
+console.log(mylength)
+
+var numberAnswer =confirm('do you want to use numbers?')
+console.log(numberAnswer)
+
+if(numberAnswer ==true){
+candidates = candidates+number
+console.log(candidates)
+}
+
+var password =''
+for(var i =0; i<mylength; i++){
+  var index= parseInt(Math.random()* candidates.length)
+password+= candidates[index]
+console.log (i, ' ',  password )
+}
+
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
